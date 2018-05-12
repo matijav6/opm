a92cdcd45401d5d3182e100a910c24c677bded21
Matija Vuk <matija.vuk.97@gmail.com>
2018-05-12 17:35:20 +0200

    home is LU

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,11 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('LU_Factorizaction', 'LUFactorization@LUHome')->name('home');
+*/
+Route::get('/', 'LUFactorization@LUHome')->name('home');
 Route::post('LU_Factorizaction/result', 'LUFactorization@Izracunaj')->name('izracunaj');
